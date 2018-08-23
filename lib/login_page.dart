479f5d84999564023e30b597d7fa7be8935a94a7
Phuvadol Thongpan <thongpan_pozn@hotmail.com>
2018-08-24 01:05:07 +0700

    first commit

---
+++ lib/login_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  Widget build(BuildContext context){
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Flutter login page'),
+      ),
+      body: new Container(
+        child: new Text('HELLO WORLD'),
+      )
+    );
+  }
+}
